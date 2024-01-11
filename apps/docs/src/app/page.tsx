93039c8f57ab518ec2b65179f86dc652b145f43f
izekki mohamed <izekki.mohamed@gmail.com>
2024-01-11 19:57:34 +0100

    atill works

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@repo/ui/src/button";
 
 export default function Page(): JSX.Element {
   return (
-    <div className=" h-screen grid  bg-card-foreground font-serif place-items-center">
+    <div className=" h-screen grid  bg-card-foreground text-3xl place-items-center">
       <Button />
     </div>
   );
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@repo/ui/src/button";
 
 export default function Page(): JSX.Element {
   return (
-    <div className="bg-primary text-secondary font-sans  h-screen grid place-items-center  bg-card-foreground ">
-      <h1>Web</h1>
+    <div className="bg-primary text-secondary h-screen grid place-items-center">
+      <h1 className="text-3xl font-bold">Web</h1>
       <Button />
     </div>
   );
