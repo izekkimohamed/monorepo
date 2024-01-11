a539a046c671cfefa7d61b6aff995a15e6206320
izekki mohamed <izekki.mohamed@gmail.com>
2024-01-11 19:45:37 +0100

    it works

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+export const Button = () => {
+  return (
+    <button className="ui-bg-green-200 ui-px-4 ui-py-2 ui-rounded-md ui-text-black ui-font-bold ui-shadow-md">
+      Boop
+    </button>
+  );
+};
