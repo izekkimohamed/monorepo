211cbcfd2ebacb94ab328960262879e7b079396d
izekki mohamed <izekki.mohamed@gmail.com>
2024-01-11 23:18:15 +0100

    edit ui tailwind.config

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
+  darkMode: ["class"],
   theme: {
-    darkMode: ["class"],
     container: {
       center: true,
       padding: "2rem",
--- packages/ui/src/components/text.tsx
+++ packages/ui/src/components/text.tsx
@@ -4,8 +4,10 @@ import { cn } from "@repo/libs/src";
 export const Test = () => {
   const active = true;
   return (
-    <div className={cn("bg-red-300", active && "bg-green-300")}>
-      test test test test
+    <div
+      className={cn("bg-red-300 p-4 rounded w-1/2", active && "bg-green-300")}
+    >
+      <h1 className="text-primary font-bold text-3xl">Test</h1>
     </div>
   );
 };
