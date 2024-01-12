4e4cee3db4ec6157e6c5d1c6eeea18d906630451
izekki mohamed <izekki.mohamed@gmail.com>
2024-01-12 01:04:55 +0100

    add prisma

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,28 @@
+import { Data } from "@repo/prisma/client";
+import { prisma } from "@repo/prisma/db";
 import { Button, Card, Input, Test } from "@repo/ui/src";
 
-export default function Page(): JSX.Element {
+export default async function Page() {
+  const data = await prisma.data.findMany();
   return (
-    <div className="bg-card-foreground text-secondary h-screen space-y-3">
+    <div className="bg-card-foreground px-6 text-secondary h-full space-y-3">
       <h1 className="text-3xl font-bold">Web</h1>
       <Button className="bg-yellow-400" size="lg">
         Web
       </Button>
-      <Card className="bg-muted p-7 rounded">
-        <Input className="bg-primary rounded" placeholder="Search" />
+      <Card className="bg-muted-foreground p-7 rounded">
+        <Input className="font-bold rounded" placeholder="Search" />
       </Card>
+      <div className="space-y-3">
+        {data.map((d) => (
+          <p
+            className="w-fit text-gray-950 font-bold text-2xl px-3 py-2 bg-slate-300 rounded"
+            key={d.id}
+          >
+            {d.libelle}
+          </p>
+        ))}
+      </div>
       <Test />
     </div>
   );
