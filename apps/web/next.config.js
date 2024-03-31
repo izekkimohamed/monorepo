ff6b8141c4714018635a8c32e9e9ec14d25f849a
izekki mohamed <izekki.mohamed@gmail.com>
2024-03-31 08:51:15 +0200

    config2

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,10 @@
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["@repo/ui", "@repo/prisma", "@repo/trpc", "@repo/libs"],
-  webpack: (config, { isServer }) => {
-    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
-    return config;
-  },
+  transpilePackages: ["@repo/ui", "@repo/prisma"],
+  // webpack: (config, { isServer }) => {
+  //   if (isServer)
+  //     config.plugins = [...config.plugins, new PrismaPlugin(process.env.DATABASE_URL)];
+  //   return config;
+  // },
 };
