cdd0d8660e5bb386032cdb5244a983698a12a767
izekki mohamed <izekki.mohamed@gmail.com>
2024-04-01 21:20:21 +0200

    client

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 
-import { trpc } from "@repo/trpc/client";
+import { useEffect, useState } from "react";
+import { getProduct } from "./actions/getProduct";
 
 export default function Page() {
-  const data = trpc.api.ticket.list.useQuery();
+  const [number, setNumber] = useState<string>("");
+  const [products, setProducts] = useState<any>();
+  const [ticket, setTicket] = useState<string>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getProduct(number);
+      if (data) {
+        setProducts(data.product);
+        console.log(data);
+      }
+    };
+    getData();
+  }, [ticket]);
   return (
-    <>
-      <div className="col-span-1 ">dashboard</div>
-      <div className="col-span-1 "> table</div>
-      {JSON.stringify(data, null, 2)}
-    </>
+    <div className="grid h-screen text-gray-100 bg-gray-800 place-items-center">
+      <div>
+        <h1 className="text-3xl font-bold">Ticket</h1>
+        {products && <pre>{JSON.stringify(products, null, 2)}</pre>}
+        <input
+          className="w-full px-4 py-2 mt-4 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+          placeholder="Enter ticket number"
+          type="text"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => setTicket(number)}
+        >
+          Set Ticket
+        </button>
+      </div>
+    </div>
   );
 }
