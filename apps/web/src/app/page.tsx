16b12822161575730b6e3769d8930d787339df7a
izekki mohamed <izekki.mohamed@gmail.com>
2024-04-01 21:37:25 +0200

    ddd

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,58 @@
-import Buttons from "@/components/buttons";
-import ListTable from "@/components/list-table";
-import Tabs from "@/components/tabs";
-import { Toaster } from "@ui/components/ui/toaster";
+// import Buttons from "@/components/buttons";
+// import ListTable from "@/components/list-table";
+// import Tabs from "@/components/tabs";
+// import { Toaster } from "@ui/components/ui/toaster";
+// export default function Page() {
+//   return (
+//     <div className="h-screen max-h-screen bg-primary">
+//       <Toaster />
+//       <div className="w-full h-full grid grid-cols-8 grid-rows-[65vh,auto] ">
+//         <ListTable />
+//         <Buttons />
+//         <Tabs />
+//       </div>
+//     </div>
+//   );
+// }
+
+"use client";
+
+import { getProduct } from "@/actions/getProduct";
+import { useEffect, useState } from "react";
+
 export default function Page() {
+  const [number, setNumber] = useState<string>("");
+  const [products, setProducts] = useState<any>();
+  const [ticket, setTicket] = useState<string>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getProduct(number);
+      if (data) {
+        setProducts(data.product);
+        console.log(data);
+      }
+    };
+    getData();
+  }, [ticket]);
   return (
-    <div className="h-screen max-h-screen bg-primary">
-      <Toaster />
-      <div className="w-full h-full grid grid-cols-8 grid-rows-[65vh,auto] ">
-        <ListTable />
-        <Buttons />
-        <Tabs />
+    <div className="grid h-screen text-gray-100 bg-gray-800 place-items-center">
+      <div>
+        <h1 className="text-3xl font-bold">Ticket</h1>
+        {products && <pre>{JSON.stringify(products, null, 2)}</pre>}
+        <input
+          className="w-full px-4 py-2 mt-4 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+          placeholder="Enter ticket number"
+          type="text"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => setTicket(number)}
+        >
+          Set Ticket
+        </button>
       </div>
     </div>
   );
