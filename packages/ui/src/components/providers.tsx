f18dd687311834fc517b74fc745d61a05e075ffb
izekki mohamed <izekki.mohamed@gmail.com>
2024-04-02 03:21:26 +0200

    prisma

--- packages/ui/src/components/providers.tsx
+++ packages/ui/src/components/providers.tsx
@@ -30,7 +30,7 @@
 
 import { AppRouter } from "@repo/trpc/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 import SuperJSON from "superjson";
@@ -44,11 +44,6 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
     api.createClient({
       transformer: SuperJSON,
       links: [
-        loggerLink({
-          enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
-        }),
         unstable_httpBatchStreamLink({
           url: getBaseUrl() + "/api/trpc",
         }),
