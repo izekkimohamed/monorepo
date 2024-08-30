1aa8190f7f69118000151b7b605bd0ae93c4a894
izekki mohamed <izekki mohamed>
2024-08-30 22:37:17 +0200

    app route

---
+++ apps/web/src/app/api/trpc/[trpc]/route.tsx
@@ -0,0 +1,15 @@
+import { appRouter } from "@repo/trpc";
+import { createTrpcContext } from "@repo/trpc/context";
+
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+const handler = (request: Request) => {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req: request,
+    router: appRouter,
+    createContext: () => createTrpcContext({ req: request }),
+  });
+};
+
+export { handler as GET, handler as POST };
--- apps/web/src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import { appRouter } from "@repo/trpc";
-import { createTrpcContext } from "@repo/trpc/context";
-import * as trpcNext from "@trpc/server/adapters/next";
-
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: async ({ req, res }) => createTrpcContext({ req, res }),
-});
